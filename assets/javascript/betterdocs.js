dd9f2798b1a9ff73f883b8e7c9a0737046c865e2
Jim Latta <jim.latta@gmail.com>
2019-07-17 10:35:29 -0400

    completed JS for doctor search, pending refactor

--- assets/javascript/betterdocs.js
+++ assets/javascript/betterdocs.js
@@ -15,51 +15,88 @@ $.ajax({
     for (var i = 0; i < newData.length; i++) {
         console.log(i)
 
-
+        // Doctor Name Variables Declared and Data Pulled from API
         var firstName = newData[i].profile.first_name;
         var lastName = newData[i].profile.last_name;
+
+        // Insurance Provider Array Defined and Data Pulled from APR
         var insuranceProvider = []
         newData[i].insurances.forEach(element => {
             insuranceProvider.push(" " + element.insurance_provider.name)
         });
         var insuranceProviderUpdated = [...new Set(insuranceProvider)]
 
+        // Doctor Address Array Defined and Data Pulled for City + State, then Merged into one field
         var doctorAddress = []
         newData[i].practices.forEach(element => {
-            doctorAddress.push(" " + element.visit_address.city + "-" + element.visit_address.state)
+            if (element.visit_address.zip !== undefined) {
+                doctorAddress.push(" " + element.visit_address.city + ", " + element.visit_address.state + " " + element.visit_address.zip);
+            } else {
+
+                doctorAddress.push(" " + element.visit_address.city + ", " + element.visit_address.state);
+            }
         });
         var doctorAddressUpdated = [...new Set(doctorAddress)]
 
-        var licenseNumber = []
+
+        // License Details pulled and combined
+        var licenseSpecs = []
         newData[i].licenses.forEach(element => {
-            licenseNumber.push(" " + element.number)
+            licenseSpecs.push(" " + element.number + "-" + element.state)
         });
-        var licenseNumberUpdated = [...new Set(licenseNumber)]
+        var licenseSpecsUpdated = [...new Set(licenseSpecs)]
 
+        // Specialty variable defined as Array and data pulled
+        var mdSpeciality = []
+        newData[i].specialties.forEach(element => {
+            mdSpeciality.push(" " + element.name)
+        });
+        var mdSpecialtyUpdated = [...new Set(mdSpeciality)]
 
+        // Rating variable defined and data pulled
+        var mdRating = []
+        newData[i].ratings.forEach(element => {
+            mdRating.push(" " + element.rating)
+        });
+        var mdRatingUpdated = [...new Set(mdRating)]
 
-        
-
-
-
+        // New Variables for List defined and set to Input Variables  
         var newList = $("<ul>");
-        // Should send to browser
         var doctorNameli = $("<li>").text("Name: " + firstName + " " + lastName);
-
-        // Should send to browser with ALL items
         var insuranceProviderli = $("<li>").text("Health Plans: " + " " + insuranceProviderUpdated);
-        // console.log(insuranceNameli);
-
-        // Should send to browser with ALL items
         var doctorAddressli = $("<li>").text("Locations: " + " " + doctorAddressUpdated);
-        // console.log(insuranceNameli);
-
-        // Should send to browser with ALL items
-        var licenseNumberli = $("<li>").text("License #: " + " " + licenseNumberUpdated);
-        console.log(licenseNumberUpdated);
+        var licenseSpecsli = $("<li>").text("License: " + " " + licenseSpecsUpdated);
+        var mdSpecialtyli = $("<li>").text("Specialties: " + " " + mdSpecialtyUpdated);
 
 
-        newList.append(doctorNameli, insuranceProviderli, doctorAddressli, licenseNumberli);
+        // This removes Empty Rating Variables
+        var mdSpecialtyli;
+        if (newData[i].specialties.length > 0) {
+            mdSpecialtyli = $("<li>").text("Specialty : " + " " + mdSpecialtyUpdated);
+        }
+        else {
+            mdSpecialtyli = "";
+        }
+
+        var mdRatingli;
+        if (newData[i].ratings.length > 0) {
+            mdRatingli = $("<li>").text("Rating : " + " " + mdRatingUpdated);
+        }
+        else {
+            mdRatingli = "";
+        }
+
+        var licenseSpecsli;
+        if (newData[i].licenses.length > 0) {
+            licenseSpecsli = $("<li>").text("License : " + " " + licenseSpecsUpdated);
+        }
+        else {
+            licenseSpecsli = "";
+        }
+
+
+        newList.append(doctorNameli, insuranceProviderli, doctorAddressli, licenseSpecsli, mdSpecialtyli, mdRatingli);
         $(".results").append(newList);
     }
 });
+
